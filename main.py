0653c90b6f8ced82b5267fc14eec244e04cb979e
ApoloLoxias <apololoxias@gmail.com>
2025-10-22 18:38:58 -0300

    writes program that counts and prints the number of words in books/frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def word_counter(text_string):
+    return(len(text_string.split()))
+
+#######################################
+
+def main(path_to_book):
+    print(f"Found {word_counter(get_book_text(path_to_book))} total words")
+
+main("books/frankenstein.txt")
