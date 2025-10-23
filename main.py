cbfde83242c59547ce5e19ce9e248e05e5735feb
ApoloLoxias <apololoxias@gmail.com>
2025-10-23 13:03:27 -0300

    adds pretty_printer function to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter, key_counter, sort_dic
+from stats import word_counter, key_counter, sort_dic, sort_alpha
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,9 @@ def list_chars(string):
     separator = "super_special_separator_that_is_highly_unlikely_to_naturaly_appear_in_a_given_written_text_'Rq9*c8;tCI9"
     return separator.join(string).split(separator)
 
+def pretty_printer(sorted_list_of_dics):
+    for dic in sorted_list_of_dics:
+        print(f"{dic["char"]}: {dic["count"]}")
 # ======================================================================
 """
 def main(path_to_book):
@@ -16,13 +19,17 @@ def main(path_to_book):
     print(key_counter(list_chars(book_text_in_lower_case)))
 """
 # =====================================================================
-
+"""
 def main(path_to_book):
     book_text_in_lower_case = get_book_text(path_to_book).lower()
     print(f"Found {word_counter(book_text_in_lower_case)} total words")
     #counted_char_dic = key_counter(list_chars(book_text_in_lower_case))
     #print(sort_dic(counted_char_dic))
-    print(sort_dic(key_counter(list_chars(book_text_in_lower_case))))
-
+    #print(sort_dic(key_counter(list_chars(book_text_in_lower_case))))
+    #print(sort_alpha(key_counter(list_chars(book_text_in_lower_case))))
+    pretty_printer(sort_alpha(key_counter(list_chars(book_text_in_lower_case))))
+"""
 #======================================================================
-main("books/frankenstein.txt")
+# main("books/frankenstein.txt")
+sorted_list_of_dics = [{"char": "a", "count": 3}, {"char": "b", "count": 2}]
+pretty_printer(sorted_list_of_dics)
